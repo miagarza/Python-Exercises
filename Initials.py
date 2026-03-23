45023443cf2eaabae1279eeac8590826704fe3c3
miagarza <138410247+miagarza@users.noreply.github.com>
2026-03-23 10:14:51 -0500

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,27 @@
+# Assignment: HW1
+# File: Initials.py
+# Student: Mia Garza
+# UT EID: mkg2545
+# Course: CS303E
+# 
+# Date: 1/15/26
+# Description of Program: Prints out the letters "MKG" with a width of 12 characters and a height of 10 lines
+
+
+print()
+print("  MMMM    MMMM      KK        KK      GGGGGGGGGGGG      ")
+print("  MM  MMMM  MM      KK      KK        GGGGGGGGGGGG      ")
+print("  MM   MM   MM      KK    KK          GG                ")
+print("  MM   MM   MM      KK  KK            GG                ")
+print("  MM   MM   MM      KKKK              GG    GGGGGG      ")
+print("  MM        MM      KKKK              GG    GGGGGG      ")
+print("  MM        MM      KK  KK            GG        GG      ")
+print("  MM        MM      KK    KK          GG        GG      ")
+print("  MM        MM  ..  KK      KK    ..  GGGGGGGGGGGG  ..  ")
+print("  MM        MM  ..  KK        KK  ..  GGGGGGGGGGGG  ..  ")
+print()
+
+
+
+
+
