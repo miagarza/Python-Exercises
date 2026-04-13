152ed18e740f6cba9298ffb0d48d3c51c2259e52
miagarza <138410247+miagarza@users.noreply.github.com>
2026-04-13 12:37:21 -0500

    Add files via upload
    monday update

---
+++ Poker.py
@@ -0,0 +1,37 @@
+# Assignment: Project2
+# File: Poker.py
+# Student: Mia Garza
+# UT EID: mkg2545
+# Course Name: CS303E
+# 
+# Date: 4/9/26
+# Description of Program: play poker with this program
+
+from Hand import *
+
+
+
+
+
+while True:
+    n=int(input("How many hands should I deal? "))
+    if n<=0:
+        print(" Positive number required. Try again! ")
+        continue
+    else:
+        d=Deck()
+        d.shuffle()
+        print()
+        break
+
+
+for i in range(1, n+1):
+    if len(d)<5:
+        print("Dealing a new deck.")
+        d=Deck()
+        d.shuffle()
+    h=Hand(d, True)
+    print("Hand drawn ("+ str(i)+ "):")
+    evaluateHand(h)
+    print()
+    
