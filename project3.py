ad13aefb0fe88dcdfee17bc3e58843d14f3fd7d2
miagarza <138410247+miagarza@users.noreply.github.com>
2026-04-20 17:38:32 -0500

    Add files via upload

--- project3.py
+++ project3.py
@@ -7,9 +7,18 @@
 # Date: 4/19/26
 # Description of Program: complex 
 
+import os
 
 
+def main():
+    filename=input("Enter a filename: ")+ ".txt"
 
+    if not os.path.exists():
+        print("File does not exist"+ filename[:-4])
+        print()
+        return
+    else:
+        createDictionary(filename)
 
 
 def cleanLine( s ):
@@ -29,7 +38,7 @@ def cleanLine( s ):
     # Use the translate() method to apply the mapping to string s
     translatedText = s.translate(translationTable)
 
-    # print("Translated Text:", translatedText)
+    #print("Translated Text:", translatedText)
     return translatedText
 
 
@@ -55,31 +64,31 @@ wordsToExclude = ['a', 'about', 'after', 'all', 'also', 'am', 'an', 'and',
 
 
 
-
-
-
-
-
-
-
-
 def createDictionary( filename ):
     """Create a dictionary associating each word in a text file with the
     number of times the word occurs.  Also count the total number of
     words and the number of unique words in the text.  Certain very
     common words are not included in the dictionary, but are counted.
     Return a triple: (wordCount, uniqueWordCount, dictionary)."""
+    FullCount=0
+    newdict={}
+
+    userfile=open(filename, "r")    #keep
+    for line in userfile:   #keep
+        newlines=cleanLine(line)    #keep
+        key=newlines.split() #keep
+
+        for word in key:    #keep
+            word=word.lower()  
+            #newdict[key.lower()]=value
+            FullCount+=1
+            if word not in wordsToExclude:
+                newdict[word]=1
+            else:
+                newdict[word]+=1
+    userfile.close()
+
+        #newdict[userfile.lower()]
 
 
-    filename=input("Text analysis of file: ")+ ".txt"
-
-    if not os.path.exists(filename):
-        print("File does not exist: "+ filename[:-4])
-        print()
-        return
-    
-
-    else:
-        userfile=open(filename, "r")
-        for line in userfile:
-            print()
+  
