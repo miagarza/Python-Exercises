5d36fe092856252fbaf0f323216485d53055809d
miagarza <138410247+miagarza@users.noreply.github.com>
2026-04-22 18:45:52 -0500

    Update RecursiveFunctions.py

--- RecursiveFunctions.py
+++ RecursiveFunctions.py
@@ -1,11 +1,4 @@
-# File: RecursiveFunctions.py
-# Student: Mia Garza
-# UT EID: mkg2545
-# Course Name: CS303E
-# 
-# Date: 4/11/26
-# Description of Program: Variety of recursive functions
-
+#a bunchhhhh of recursive functions
 
 
 def sumItemsInList( L ):
